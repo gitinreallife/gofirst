48345fff0193b575ed1c4f00a064b490793313ec
Gita An <gitaaanisa@gmail.com>
2021-05-04 17:35:15 +0700

    array splice pointers

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.16
 replace github.com/gitinreallife/greetings => ../greetings
 
 require (
+	github.com/gitinreallife/govars v0.0.0-00010101000000-000000000000
 	github.com/gitinreallife/greetings v0.0.0-00010101000000-000000000000
 	github.com/gitinreallife/morestrings v0.0.0-20210429103826-099f29569121
 )
+
+replace github.com/gitinreallife/govars => ../govars
--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gitinreallife/greetings"
 	"github.com/gitinreallife/morestrings"
-	// go mod edit --replace=github.com/gitinreallife/greetings=../greetings
+	//  go mod edit --replace=github.com/gitinreallife/govars=../govars
 )
+
+// go mod init gofirst(package name)
+func main() {
+
+	fmt.Println("##################################################")
+	PrintArrays()
+	// PrintVars()
+	fmt.Println("##################################################")
+	log.SetPrefix("Exception greetings: ")
+	log.SetFlags(0)
+	name := ""
+	fmt.Println(("What's your name? "))
+	fmt.Scanln(&name)
+	strQuote, err := greetings.GetQuote(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("#####Calling Greeting Module")
+	fmt.Println(strQuote)
+	fmt.Println("#####Calling Reverse Module")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+
+}
 
 const myConst int = 40
 const (
@@ -22,10 +46,71 @@ const (
 	KB2 = iota
 )
 
-// go mod init gofirst(package name)
-func main() {
+func PrintArrays() {
+	fmt.Println("###############1 D Array Playground###############")
 
-	fmt.Println("##################################################")
+	grades := [3]int{97, 85, 93}
+	fmt.Printf("Grades: %v\n", grades)
+
+	var students [3]string
+	fmt.Printf("Students: %v\n", students)
+	students[0] = "Lisa"
+	students[1] = "Jisoo"
+	students[2] = "Rosé"
+	fmt.Printf("Students: %v\n", students)
+
+	fmt.Println("###############Multidimentional array Playground###############")
+	var idMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
+
+	//or declare it like this
+	idMatrix[0] = [3]int{1, 0, 0}
+	idMatrix[1] = [3]int{0, 1, 0}
+	idMatrix[2] = [3]int{0, 0, 1}
+
+	fmt.Println(idMatrix)
+
+	fmt.Println("############### Pointer array Playground###############")
+	//start pointer array
+	a := [...]int{1, 2, 3}
+	b := &a
+	b[1] = 5
+	fmt.Println(a)
+	fmt.Println(b)
+
+	fmt.Println("############### Slice array Playground###############")
+	//slice
+	sliceA := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("Slice Length: " + strconv.Itoa(len(sliceA)))
+	fmt.Println("Slice Capacity: " + strconv.Itoa(cap(sliceA)))
+
+	sliceB := sliceA[:]   //slice all elements
+	sliceC := sliceB[3:]  //slice from 4th element to end
+	sliceD := sliceB[:6]  //slice untill 6th elements
+	sliceE := sliceB[3:6] //slice from 4-6
+	sliceA[5] = 42
+	fmt.Println(sliceA)
+	fmt.Println(sliceB)
+	fmt.Println(sliceC)
+	fmt.Println(sliceD)
+	fmt.Println(sliceE)
+
+	makeA := make([]int, 3, 6) //create slice len 3 cap 6
+	makeA = append(makeA, 1)
+	makeA = append(makeA, []int{2, 3, 4, 5}...)
+	fmt.Println(makeA)
+	fmt.Printf("Length Make: %v\n", len(makeA))
+	fmt.Printf("Capacity Make: %v\n", cap(makeA))
+
+	//removing slice
+	makeB := makeA[3:8]
+	fmt.Println(makeB)
+
+	makeC := append(makeB[:1], makeB[2:]...)
+	fmt.Println(makeC)
+}
+
+func PrintVars() {
+
 	fmt.Println("###############Variables Playground###############")
 	fmt.Println("#####bool")
 	n := 1 == 1
@@ -61,7 +146,7 @@ func main() {
 	filesize := 30000000.
 	fmt.Printf("%.2fGB \n", filesize/GB)
 
-	fmt.Println("-----Roles bit------")
+	fmt.Println("-----Roles bitwise & bitshifting------")
 	const (
 		isAdmin = 1 << iota
 		isHQ
@@ -76,19 +161,4 @@ func main() {
 	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
 	fmt.Println("###############End ofConst Playground##########")
 	fmt.Println("###############################################")
-
-	log.SetPrefix("Exception greetings: ")
-	log.SetFlags(0)
-	name := ""
-	fmt.Println(("What's your name? "))
-	fmt.Scanln(&name)
-	strQuote, err := greetings.GetQuote(name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("#####Calling Greeting Module")
-	fmt.Println(strQuote)
-	fmt.Println("#####Calling Reverse Module")
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-
 }
